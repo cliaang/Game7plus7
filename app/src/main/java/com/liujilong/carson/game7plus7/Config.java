28bee0fef47f4bcb363f46c9e59f70a06aaf66a2
cliaang <1538828451@qq.com>
2015-03-18 21:00:03 +0800

    add atyGame

---
+++ app/src/main/java/com/liujilong/carson/game7plus7/Config.java
@@ -0,0 +1,10 @@
+package com.liujilong.carson.game7plus7;
+
+/**
+ * Created by Administrator on 2015/3/18 0018.
+ */
+public class Config {
+    public static final String START_OR_RESUME_GAME = "com.liujilong.carson.game7plus7.startorresume";
+    public static final int START_GAME = 1;
+    public static final int RESUME_GAME = 2;
+}
--- app/src/main/java/com/liujilong/carson/game7plus7/atyWelCome.java
+++ app/src/main/java/com/liujilong/carson/game7plus7/atyWelCome.java
@@ -1,39 +1,43 @@
 package com.liujilong.carson.game7plus7;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
-
-public class atyWelCome extends ActionBarActivity {
-
+public class atyWelCome extends Activity {
+    private boolean hasSaved = true;
+    private Button btnResume;
+    private Button btnNewGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置无标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_aty_wel_come);
+        btnResume= (Button)findViewById(R.id.button_resume_game);
+        btnNewGame = (Button)findViewById(R.id.button_new_game);
+        btnNewGame.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (hasSaved){
+                    DialogFragment df = new fargConfirmNewGame();
+                    df.show(getFragmentManager(),"new game");
+                } else {
+                    startGame(Config.START_GAME);
+                }
+            }
+        });
+    }
+    //inputMessage :
+    //Config.START_GAME Config.RESUME_GAME
+    public  void startGame(int inputMessage){
+        Intent i = new Intent(atyWelCome.this,atyGame.class);
+        i.putExtra(Config.START_OR_RESUME_GAME,inputMessage);
+        startActivity(i);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_aty_wel_come, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
